d528fb893cd2d6f0e70ef4dfeec8569ccdb4abfc
Tim G. Thomas <tim@timgthomas.com>
2019-05-18 17:23:33 -0500

    add luxury goods vendor command

--- scripts/eso.js
+++ scripts/eso.js
@@ -1,13 +1,43 @@
 const IcalExpander = require('ical-expander')
 const request = require('request')
 
-const url = 'https://calendar.google.com/calendar/ical/stdv9h2f0amjg8gf6p3ekvsjug%40group.calendar.google.com/public/basic.ics'
+const luxuryUrl = 'https://calendar.google.com/calendar/ical/e97ku4olpreln13p1rq43bd6p4%40group.calendar.google.com/public/basic.ics'
+const pledgesUrl = 'https://calendar.google.com/calendar/ical/stdv9h2f0amjg8gf6p3ekvsjug%40group.calendar.google.com/public/basic.ics'
 
 module.exports = robot => {
 
+  robot.hear(/luxury/i, msg => {
+
+    request(luxuryUrl, (err, res, body) => {
+
+      if (err) {
+        msg.send('Sorry, something went wrong getting this weekend’s luxury items. :(')
+        return
+      }
+
+      let ical = new IcalExpander({ ics: body })
+      // TODO: If it's not a weekend today, grab the following weekend's goods.
+      let { occurrences } = ical.between(new Date(), new Date())
+      if (occurrences.length === 0) {
+        msg.send('Nothing available at the moment. Check back again soon!')
+        return
+      }
+
+      let items = occurrences[0].item.description
+      items = items.match(/^.*\d+,\d+g$/gm).map(x => {
+        let split = x.lastIndexOf(' ')
+        return { item: x.substring(0, split), price: x.substring(split + 1) }
+      })
+
+      msg.send(`This weekend’s luxury items:${items.map(x => '\n• *' + x.item + '*: ' + x.price).join('')}`)
+
+    })
+
+  })
+
   robot.hear(/pledges/i, msg => {
 
-    request(url, (err, res, body) => {
+    request(pledgesUrl, (err, res, body) => {
 
       if (err) {
         msg.send('Sorry, something went wrong getting today’s pledges. :(')
@@ -18,7 +48,7 @@ module.exports = robot => {
       let { occurrences } = ical.between(new Date(), new Date())
       let pledges = occurrences.map(x => ({ summary: x.item.summary, description: x.item.description.trim() }))
 
-      msg.send(`Today’s pledges are below. We are Undaunted!${pledges.map(x => '\n• ' + x.summary + ' (' + x.description + ')').join('')}`)
+      msg.send(`Today’s pledges are below. We are Undaunted!${pledges.map(x => '\n• *' + x.summary + '* (' + x.description + ')').join('')}`)
 
     })
 
