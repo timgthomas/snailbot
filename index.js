973f068b6da254e4cbb42a3cf4d6767b2001cb27
Tim G. Thomas <tim@timgthomas.com>
2023-05-06 01:10:44 -0500

    [wip] import commands from folder

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
-import { Client, Events, GatewayIntentBits, REST, Routes, SlashCommandBuilder } from 'discord.js'
+import {
+  Client,
+  Events,
+  GatewayIntentBits,
+  REST,
+  Routes,
+  SlashCommandBuilder,
+} from 'discord.js'
 import dotenv from 'dotenv'
+import getCommands from './lib/get-commands.js'
 
 dotenv.config()
 const clientId = process.env.DISCORD_CLIENT_ID
@@ -7,46 +15,50 @@ const token = process.env.DISCORD_TOKEN
 
 const client = new Client({ intents: [ GatewayIntentBits.Guilds ] })
 
-const testCommand = {
-  data: new SlashCommandBuilder()
-    .setName('ping')
-    .setDescription('.'),
-  execute: ({ reply }) => {
-    reply('pong!')
-  },
-}
-
-client.once('ready', async (e) => {
+client.once(Events.ClientReady, async (e) => {
   console.log(`Ready! Logged in as ${e.user.tag}`)
+
+  client.commands = []
+
+  for await (const command of getCommands()) {
+    client.commands[command.name] = {
+      ...command,
+      data: new SlashCommandBuilder()
+        .setName(command.name)
+        .setDescription(command.description)
+        .toJSON()
+    }
+  }
+
+  const commandsJson = Object.values(client.commands).map(({ data }) => data)
 
   await new REST().setToken(token).put(
     Routes.applicationCommands(clientId),
-    { body: [ testCommand.data.toJSON() ] },
+    { body: commandsJson },
   )
 })
 
-client.on('interactionCreate', async (interaction) => {
-	if (!interaction.isChatInputCommand()) return
+client.on(Events.InteractionCreate, async (interaction) => {
+  if (!interaction.isChatInputCommand()) return
 
-  console.log('interaction', interaction)
+  const command = interaction.client.commands[interaction.commandName]
 
-	const command = testCommand // interaction.client.commands.get(interaction.commandName)
+  if (!command) {
+    console.error(`No command matching ${interaction.commandName} was found.`)
+    return
+  }
 
-	if (!command) {
-		console.error(`No command matching ${interaction.commandName} was found.`)
-		return
-	}
-
-	try {
-		await command.execute.call(interaction, { reply: interaction.reply.bind(interaction) })
-	} catch (error) {
-		console.error(error)
-		if (interaction.replied || interaction.deferred) {
-			await interaction.followUp({ content: 'There was an error while executing this command!', ephemeral: true })
-		} else {
-			await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true })
-		}
-	}
+  try {
+    await command.execute.call(interaction, { reply: interaction.reply.bind(interaction) })
+    console.log(`[command] handled command: ${interaction.commandName}`)
+  } catch (error) {
+    console.error(`[command] error handling command: ${interaction.commandName} (${error})`)
+    if (interaction.replied || interaction.deferred) {
+      await interaction.followUp({ content: 'There was an error while executing this command!', ephemeral: true })
+    } else {
+      await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true })
+    }
+  }
 })
 
 client.login(token)
